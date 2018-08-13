91ae4fbbde2fe4bb60fd1b452d6532552118137e
Guest User <Guest@epicodus-26.local>
2018-08-13 07:44:58 -0700

    It will add updated files to the .js.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,46 @@ $(document).ready(function() {
 });
 
 
-// var leapYear = function(year) {   // jQuery user interface logic here.
+// var leapYear = function(year) {   // Repeat: jQuery user interface logic here.
 //   return false;
 // };
 //
+
+
+// var leapYear = function(year) {  //Refactoring: jQuery user interface logic here.
+//   if (year % 100 === 0) {
+//     return false;
+//   } else if (year % 4 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// var leapYear = function(year) {  // jQuery user interface logic here.
+//   if (year % 100 === 0) {
+//     return false;
+//   } else if (year % 4 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+
+
+
+// var leapYear = function(year) {   // jQuery user interface logic here.
+//   if ((year % 4 === 0) && (year % 100 !== 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// var leapYear = function(year) {    //Implementing Additional Behaviors: jQuery user interface logic here.
+//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
